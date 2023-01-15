d77c984fff181075005e42c80c1feca473fd0cad
rafialfian10 <rafialfian770@gmail.com>
2023-01-15 15:38:09 +0700

    backend

--- handlers/transaction.go
+++ handlers/transaction.go
@@ -1,13 +1,17 @@
 package handlers
 
 import (
+	"crypto/tls"
 	"encoding/json"
+	"fmt"
+	"log"
 
 	"net/http"
 	"os"
 	"strconv"
 	dto "waysbook/dto/result"
 	transactiondto "waysbook/dto/transaction"
+	"waysbook/models"
 
 	"waysbook/repositories"
 
@@ -15,6 +19,7 @@ import (
 	"github.com/midtrans/midtrans-go"
 	"github.com/midtrans/midtrans-go/coreapi"
 	"github.com/midtrans/midtrans-go/snap"
+	"gopkg.in/gomail.v2"
 )
 
 type handlerTransaction struct {
@@ -144,31 +149,91 @@ func (h *handlerTransaction) Notification(w http.ResponseWriter, r *http.Request
 
 	if transactionStatus == "capture" {
 		if fraudStatus == "challenge" {
+			SendEmail("FAILED", transaction)
 			// TODO set transaction status on your database to 'challenge'
 			// e.g: 'Payment status challenged. Please take action on your Merchant Administration Portal
 			h.TransactionRepository.UpdateTrans("pending", transaction.Id)
 		} else if fraudStatus == "accept" {
+			SendEmail("OK", transaction)
 			// TODO set transaction status on your database to 'success'
 			// SendMail("success", transaction)
 			h.TransactionRepository.UpdateTrans("success", transaction.Id)
 		}
 	} else if transactionStatus == "settlement" {
+		SendEmail("OK", transaction)
 		// TODO set transaction status on your databaase to 'success'
 		// SendMail("success", transaction)
 		h.TransactionRepository.UpdateTrans("success", transaction.Id)
 	} else if transactionStatus == "deny" {
+		SendEmail("FAILED", transaction)
 		// TODO you can ignore 'deny', because most of the time it allows payment retries
 		// and later can become success
 		// SendMail("failed", transaction)
 		h.TransactionRepository.UpdateTrans("failed", transaction.Id)
 	} else if transactionStatus == "cancel" || transactionStatus == "expire" {
+		SendEmail("FAILED", transaction)
 		// TODO set transaction status on your databaase to 'failure'
 		// SendMail("failed", transaction)
 		h.TransactionRepository.UpdateTrans("failed", transaction.Id)
 	} else if transactionStatus == "pending" {
+		SendEmail("WAITING", transaction)
 		// TODO set transaction status on your databaase to 'pending' / waiting payment
 		h.TransactionRepository.UpdateTrans("pending", transaction.Id)
 	}
 
 	w.WriteHeader(http.StatusOK)
+}
+
+// function send email
+func SendEmail(status string, transaction models.Transaction) {
+	var CONFIG_SMTP_HOST = "smtp.gmail.com"
+	var CONFIG_SMTP_PORT = 587
+	var CONFIG_SENDER_NAME = "waysbook <rafialfian770@gmail.com>"
+	var CONFIG_AUTH_EMAIL = os.Getenv("SYSTEM_EMAIL")
+	var CONFIG_AUTH_PASSWORD = os.Getenv("SYSTEM_PASSWORD")
+
+	var BookName = transaction.User.Name
+	var price = strconv.Itoa(transaction.Total)
+
+	mailer := gomail.NewMessage()
+	mailer.SetHeader("From", CONFIG_SENDER_NAME)
+	mailer.SetHeader("To", transaction.User.Email)
+	mailer.SetHeader("Subject", "Status Transaction")
+	mailer.SetBody("text/html", fmt.Sprintf(`<!DOCTYPE html>
+    <html lang="en">
+      <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Document</title>
+      <style>
+        h1 {
+        color: brown;
+        }
+      </style>
+      </head>
+      <body>
+      <h2>Product payment :</h2>
+      <ul style="list-style-type:none;">
+        <li>Name : %s</li>
+        <li>Total payment: Rp.%s</li>
+        <li>Status : %s</li>
+		<li>Iklan : %s</li>
+      </ul>
+      </body>
+    </html>`, BookName, price, status, "Terima kasih"))
+
+	dialer := gomail.NewDialer(
+		CONFIG_SMTP_HOST,
+		CONFIG_SMTP_PORT,
+		CONFIG_AUTH_EMAIL,
+		CONFIG_AUTH_PASSWORD,
+	)
+
+	dialer.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+	err := dialer.DialAndSend(mailer)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
